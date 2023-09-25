ba5f698ab59e9228130e9eb2d10930618f54b966
darfixx <darfi.abdllah@gmail.com>
2023-09-25 01:52:33 -0400

    added Edittext, Button, and Textview

--- app/src/main/java/algonquin/cst2335/darf0003/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/darf0003/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.darf0003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.mytextview);
+        Button b = findViewById(R.id.mybutton);
+        EditText et = findViewById(R.id.mytext);
+
+        b.setOnClickListener( v-> {
+
+                tv.setText("you clicked the button");
+                b.setText("you clicked the button");
+                et.setText("you clicked the button");
+        });
+
     }
 }
